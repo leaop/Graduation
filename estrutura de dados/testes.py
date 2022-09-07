6c217d0d31b006e61cee3e4d66e064bfe2a6cdd0
leaop <leaoneto_7@hotmail.com>
2022-09-07 20:11:06 -0300

    Update testes.py

--- estrutura de dados/testes.py
+++ estrutura de dados/testes.py
@@ -1,8 +1,1 @@
 #sempre com testes aleatórios
-def main ():
-    seq[4,3,2,1]
-    pilha = []
-    for elemento in seq:
-        pilha.append(elemento)
-    while len(pilha)>0:
-        print(pilha)
